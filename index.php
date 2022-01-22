cda72cf32050dc9ef24cbe00fa0b543fdce9e448
sysmaster <sysmaster@gmail.com>
2022-01-22 05:50:41 +0200

    Oggi!

---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans:wght@600&family=Roboto&display=swap"
+          rel="stylesheet">
+    <title>Cant See its?</title>
+</head>
+<body>
+<!-- рамки екрана -->
+<div class="header">
+
+<!-- головна строрінка -->
+    <div class="info">
+
+    <!-- блок с інформацией -->
+        <div class="container">
+        <div class=" info__contact ">
+            <h4 class="contact__calendar">
+                ПН-СБ: 09:00–19:00
+            </h4>
+            <h4 class="contact__number">
+                +38 (096) 812 00 13
+            </h4>
+            <h4 class="languages">
+                МОВА: УКР
+                <a class="keyFrame1">&#9660;</a>
+            </h4>
+            <h4 class="currency">
+                ВАЛЮТА: uah
+                <a class="keyFrame2">&#9660;</a>
+            </h4>
+        </div>
+
+</div>
+
+<!-- логотип та Меню-->
+        <div class="container">
+        <div class="header__inner">
+            <div class="header__logo">CreWW</div>
+            <div class="nav text" >
+                <a class="nav__link" href="#">одяг</a>
+                <a class="nav__link" href="#">медицина</a>
+                <a class="nav__link" href="#">підсумки</a>
+                <a class="nav__link" href="#">рюкзаки</a>
+                <a class="nav__link" href="#">розгрузки</a>
+            </div>
+            <div class="nav__svg">
+                <a class="nav__img" href="#">1</a>
+                <a class="nav__img" href="#">2</a>
+                <a class="nav__img" href="#">3</a>
+                <a class="nav__img" href="#">4</a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+<!-- Заставка-->
+    <div class="br__body">
+        <span class="gray"></span>
+        <div class="container">
+             <a class="br__tag__left"> / - -</a>
+             <button class="br__button" >переглянути новинки</button>
+             <a class="br__tag__ring"> - - /</a>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+<!-- закритя тега  header --> </div>
+</body>
+
+</html>
